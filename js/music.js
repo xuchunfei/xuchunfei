8a33ab6f278c3e577dd00d80c7dc0950aaa1585c
xuchunfei <2367347407@qq.com>
2024-11-16 14:51:44 +0800

    2024.11.16 version-3.3

--- js/music.js
+++ js/music.js
@@ -1,5 +1,6 @@
-lyric=["早已熄灯 万籁俱寂","思考着一个问题" ,"认识你的人那么多","无不称你牛逼","熟悉又陌生的孙开","谱写传奇",
-    "那样的你 是如此","让爱开为你着迷"];
-for(var i=0;i<lyric.length;i++){
-    document.write(lyric[i])
+function buttomClick_1(){
+    alert("制作中，敬请期待！目前进度：作曲");
+}
+function buttomClick_2(){
+    alert("收到，必定爆肝制作！");
 }
